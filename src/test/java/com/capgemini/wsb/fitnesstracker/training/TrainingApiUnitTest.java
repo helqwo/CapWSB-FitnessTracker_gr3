c1c6086c8eb67aee15bd6dee7b1de0d22576b647
Helqu <73479846+helqwo@users.noreply.github.com>
2024-11-22 12:02:08 +0100

    added test file

---
+++ src/test/java/com/capgemini/wsb/fitnesstracker/training/TrainingApiUnitTest.java
@@ -0,0 +1,10 @@
+package com.capgemini.wsb.fitnesstracker.training;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrainingApiUnitTest {
+
+    //stworzyć testy jednostkowe do funkcji które stworzyłyśmy w lab3
+    //pokrycie >80% - tyle procent linijek kodu, somehow można sprawdzić w mavean->test --> czemu nie działa?
+}
